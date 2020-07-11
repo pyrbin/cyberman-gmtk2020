f1956af1c6c902ca7d304565262a7ad32895f4a9
carlo826 <carllorentzon@hotmail.se>
2020-07-11 23:14:43 +0200

    Working on mana logic

---
+++ Assets/Scripts/ManaRefiller.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class ManaRefiller : MonoBehaviour
+{
+    [ReorderableList]
+    [SerializeField]
+    public List<ManaUnitScript> ManaUnitList;
+
+    [SerializeField]
+    private int currentMana;
+
+    private List<ManaUnitScript> EmptyManaUnitList;
+    private List<ManaUnitScript> FilledManaUnitList;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (ManaUnitScript manaUnit in ManaUnitList){
+            if (manaUnit.isFilled)
+            {
+                FilledManaUnitList.Add(manaUnit);
+            }
+            else
+            {
+                EmptyManaUnitList.Add(manaUnit);
+            }
+        }
+        currentMana = FilledManaUnitList.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void removeMana(int amount){
+        if (amount > currentMana)
+            return;
+        for (int i = FilledManaUnitList.Count-1; i >= 0; i--){
+            ManaUnitScript manaPip = FilledManaUnitList[i];
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ManaUnitScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaUnitScript : MonoBehaviour
+{
+    public bool isFilled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFilled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
