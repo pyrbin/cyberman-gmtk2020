33d364a0944055251c75e407f057ca3691a3389e
carlo826 <carllorentzon@hotmail.se>
2020-07-12 00:02:00 +0200

    Finished ManaRefiller

--- Assets/Scripts/ManaRefiller.cs
+++ Assets/Scripts/ManaRefiller.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class ManaRefiller : MonoBehaviour
 {
@@ -12,8 +13,12 @@ public class ManaRefiller : MonoBehaviour
     [SerializeField]
     private int currentMana;
 
-    private List<ManaUnitScript> EmptyManaUnitList;
-    private List<ManaUnitScript> FilledManaUnitList;
+    [SerializeField]
+    public Sprite emptyPipImage;
+    public Sprite filledPipImage;
+
+    private List<ManaUnitScript> EmptyManaUnitList = new List<ManaUnitScript>{};
+    private List<ManaUnitScript> FilledManaUnitList = new List<ManaUnitScript>{};
 
     // Start is called before the first frame update
     void Awake()
@@ -39,9 +44,23 @@ public class ManaRefiller : MonoBehaviour
     void removeMana(int amount){
         if (amount > currentMana)
             return;
-        for (int i = FilledManaUnitList.Count-1; i >= 0; i--){
+        for (int i = FilledManaUnitList.Count-1; i >= 0; i--)
+        {
             ManaUnitScript manaPip = FilledManaUnitList[i];
-            FilledManaUnitList
+            manaPip.isFilled = false;
+            manaPip.gameObject.transform.GetComponent<Image>().sprite = emptyPipImage;
+            FilledManaUnitList.RemoveAt(i);
+            EmptyManaUnitList.Add(manaPip);
+        }
+        currentMana -= amount;
+    }
+    void addMana(int amount){
+        foreach (ManaUnitScript manaPip in EmptyManaUnitList)
+        {
+            manaPip.isFilled = true;
+            manaPip.gameObject.transform.GetComponent<Image>().sprite = filledPipImage;
+            EmptyManaUnitList.Remove(manaPip);
+            FilledManaUnitList.Add(manaPip);
         }
     }
 }
